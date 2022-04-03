088b1baa74ec4448fa8b64db10d40d159a66b98c
teregonzalez <teregonzalezpro@gmail.com>
2022-04-03 17:58:02 -0400

    Se crea funcion verifyRoot y su test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = () => {
-  // ...
+// const { path } = require('path');
+const fs = require('fs');
+
+const verifyRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
+
+module.exports = {
+  verifyRoot
 };
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,22 @@
-const mdLinks = require('../');
+// const mdLinks = require('../');
+const {verifyRoot} = require('../index')
 
 
-describe('mdLinks', () => {
+describe('test in verifyRoot', () => {
+  it('should return "El archivo EXISTE!" for README.md', () => {
+    const ruta = './README.md'
 
-  it('should...', () => {
-    console.log('FIX ME!');
+    const result = verifyRoot(ruta);
+
+    expect(result).toEqual('El archivo EXISTE!');
+  });
+
+  it('should return "El archivo NO EXISTE!" for README.js', () => {
+    const ruta = './README.js'
+
+    const result = verifyRoot(ruta);
+
+    expect(result).toEqual('El archivo NO EXISTE!');
   });
 
 });
