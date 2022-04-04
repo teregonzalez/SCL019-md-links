c46d51b63b05a8fe5dd6dbbd12218e07e4a7801f
teregonzalez <teregonzalezpro@gmail.com>
2022-04-04 14:42:29 -0400

    Se completa Ingresa ruta. Close #1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // const { path } = require('path');
 const fs = require('fs');
+const readline = require('readline')
 
-const verifyRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
+const getInput = require('readline').createInterface({   // crea input para que el cliente ingrese la ruta
+  input:process.stdin,
+  output:process.stdout
+})
 
-module.exports = {
-  verifyRoot
-};
+getInput.question('Ingresa tu ruta:', ruta = () => {
+  let routerinsert = `${ruta}`;
+})
 
+
+// const verifyRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
+
+// module.exports = {
+//   verifyRoot
+// };
+
+// process.stdout.write('Holaaa')
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,21 +2,21 @@
 const {verifyRoot} = require('../index')
 
 
-describe('test in verifyRoot', () => {
-  it('should return "El archivo EXISTE!" for README.md', () => {
-    const ruta = './README.md'
+// describe('test in verifyRoot', () => {
+//   it('should return "El archivo EXISTE!" for README.md', () => {
+//     const ruta = './README.md'
 
-    const result = verifyRoot(ruta);
+//     const result = verifyRoot(ruta);
 
-    expect(result).toEqual('El archivo EXISTE!');
-  });
+//     expect(result).toEqual('El archivo EXISTE!');
+//   });
 
-  it('should return "El archivo NO EXISTE!" for README.js', () => {
-    const ruta = './README.js'
+//   it('should return "El archivo NO EXISTE!" for README.js', () => {
+//     const ruta = './README.js'
 
-    const result = verifyRoot(ruta);
+//     const result = verifyRoot(ruta);
 
-    expect(result).toEqual('El archivo NO EXISTE!');
-  });
+//     expect(result).toEqual('El archivo NO EXISTE!');
+//   });
 
-});
+// });
