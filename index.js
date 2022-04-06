cfde4e5abffd3d98328f0393918c37e0a3bb2283
teregonzalez <teregonzalezpro@gmail.com>
2022-04-05 22:56:36 -0400

    Se completa Revisar ruta. Close #5

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // const { path } = require('path');
 const fs = require('fs');
 const readline = require('readline')
+const path = require('path');
 
-const getInput = require('readline').createInterface({   // crea input para que el cliente ingrese la ruta
+const getInput = require('readline').createInterface({
   input:process.stdin,
   output:process.stdout
 })
 
-getInput.question('Ingresa tu ruta:', ruta = () => {
-  let routerinsert = `${ruta}`;
+getInput.question('Ingresa tu ruta:', (ruta) => {
+  const absolutePath = (ruta) => path.isAbsolute(ruta);
+  // === true ? "Es absoluta" : "No es absoluta";
+  console.log(absolutePath(ruta));
+getInput.close()
 })
+
+
+//  fs.readFile(routerinsert)
 
 
 // const verifyRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
