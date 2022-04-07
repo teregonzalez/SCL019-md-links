42601993ccf0878ea8e08a74eb675f3d0bcf8aac
teregonzalez <teregonzalezpro@gmail.com>
2022-04-07 12:08:51 -0400

    Se agrega test

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const fs = require('fs');
 const readline = require('readline')
 const path = require('path');
 
+const { existRoot, verifyAbsolute, transformAbsolute } = require('./verifyFunctions')
+
 const getInput = require('readline').createInterface({
   input:process.stdin,
   output:process.stdout
 })
 
-getInput.question('Ingresa tu ruta:', (ruta) => {
-  // const absolutePath = (ruta) => path.isAbsolute(ruta);
-  // // === true ? "Es absoluta" : "No es absoluta";
-  // console.log(absolutePath(ruta));
+getInput.question('Ingresa tu ruta: ', (ruta) => {
+
+  if(existRoot(ruta)){
+    verifyAbsolute(ruta) ? ruta : transformAbsolute(ruta);
+  }else{
+    process.stdout.write('No existe') 
+  }
+  
 getInput.close()
 })
 
@@ -19,7 +25,7 @@ getInput.close()
 //  fs.readFile(routerinsert)
 
 
-// const verifyRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
+// const existRoot = (ruta) => fs.existsSync(ruta) === true ? "El archivo EXISTE!" : "El archivo NO EXISTE!";
 
 // module.exports = {
 //   verifyRoot
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,41 +1,69 @@
 // const mdLinks = require('../');
 // const {verifyRoot} = require('../index')
-const {absolutePath} = require('../verifyFunctions')
+const {existRoot, verifyAbsolute, transformAbsolute} = require('../verifyFunctions')
 
 
 // describe('test in verifyRoot', () => {
-//   it('should return "El archivo EXISTE!" for README.md', () => {
+// it('should return "El archivo EXISTE!" for README.md', () => {
 //     const ruta = './README.md'
 
 //     const result = verifyRoot(ruta);
 
 //     expect(result).toEqual('El archivo EXISTE!');
-//   });
+// });
 
-//   it('should return "El archivo NO EXISTE!" for README.js', () => {
+// it('should return "El archivo NO EXISTE!" for README.js', () => {
 //     const ruta = './README.js'
 
 //     const result = verifyRoot(ruta);
 
 //     expect(result).toEqual('El archivo NO EXISTE!');
-//   });
+// });
 
 // });
 
-describe('test in absolutePath', () => {
-  it('should return "true" for "C:\Users\Gamer\Documents\Laboratoria\SCL019-md-links\README.md"', () => {
-    const ruta = 'C:\\Users\\Gamer\\Documents\\Laboratoria\\SCL019-md-links\\README.md'
+describe('test in verifyAbsolute', () => {
+    it('should return "true" for "C:\Users\Gamer\Documents\Laboratoria\SCL019-md-links\README.md"', () => {
+        const ruta = 'C:\\Users\\Gamer\\Documents\\Laboratoria\\SCL019-md-links\\README.md'
 
-    const result = absolutePath(ruta);
+        const result = verifyAbsolute(ruta);
 
-    expect(result).toBe(true);
-  });
+        expect(result).toBe(true);
+    });
 
-  it('should return "false" for package.json', () => {
-    const ruta = 'package.json'
+    it('should return "false" for package.json', () => {
+        const ruta = 'package.json'
 
-    const result = absolutePath(ruta);
+        const result = verifyAbsolute(ruta);
 
-    expect(result).toBe(false);
+        expect(result).toBe(false);
+    });
+});
+
+
+describe('test in transformAbsolute', () => {
+    it('should return "C:\\Users\\Gamer\\Documents\\Laboratoria\\SCL019-md-links\\package.json" for package.json', () => {
+        const ruta = 'package.json'
+
+        const result = transformAbsolute(ruta);
+
+        expect(result).toBe("C:\\Users\\Gamer\\Documents\\Laboratoria\\SCL019-md-links\\package.json");
+    });
+});
+
+describe('test in existRoot', () => {
+    it('should return "true" for package.json', () => {
+        const ruta = 'package.json'
+
+        const result = existRoot(ruta);
+
+        expect(result).toBe(true);
+    });
+    it('should return "false" for package.json', () => {
+      const ruta = 'fakeFile.json'
+
+      const result = existRoot(ruta);
+
+      expect(result).toBe(false);
   });
 });
